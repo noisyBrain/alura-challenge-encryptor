d69a25baccb30aa85b6a3a5fef01991a0ab4c5fc
Tomas <tomasarreguib@gmail.com>
2022-08-11 16:29:25 -0300

    Add event listeners to encrypt and decrypt buttons. Added show and hide feature to aside img

---
+++ index.js
@@ -0,0 +1,88 @@
+const formTextarea = document.getElementById("form__textarea");
+const asideTextarea = document.getElementById("aside__textarea");
+const encryptButton = document.getElementById("article__encrypt-btn");
+const decryptButton = document.getElementById("article__decrypt-btn");
+const munhecoImg = document.getElementById("aside__munheco");
+const asideTitle = document.getElementById("aside__title");
+const asideDescription = document.getElementById("aside__description");
+const asideClipboard = document.getElementById("aside__clipboard");
+
+// Funcion para encritar el mensaje
+function encryptMessage (e) {
+  let word = e.value.replace(
+    // Tomo las posibles opciones
+    /(a)|(e)|(i)|(o)|(u)/g,
+    (_, regex1, regex2, regex3, regex4, regex5) => {
+     // comprubo, si matchea, remplazo por el string
+     if (regex1) return "ai"
+     if (regex2) return "enter"
+     if (regex3) return "imes"
+     if (regex4) return "ober"
+     if (regex5) return "ufat"
+    }
+  )
+  // seteo el texto del textarea con lo que devuelva
+  // el método replace
+  asideTextarea.innerHTML = word
+}
+
+// Function para desencriptar el mensaje
+function decryptMessage (e) {
+  let word = e.value.replace(
+    // Tomo las posibles opciones
+    /(ai)|(enter)|(imes)|(ober)|(ufat)/g,
+    (_, regex1, regex2, regex3, regex4, regex5) => {
+     // comprubo, si matchea, remplazo por el string
+     if (regex1) return "a"
+     if (regex2) return "e"
+     if (regex3) return "i"
+     if (regex4) return "o"
+     if (regex5) return "u"
+    }
+  )
+  // seteo el texto del textarea con lo que devuelva
+  // el método replace
+  asideTextarea.innerHTML = word
+}
+
+// Function que oculta los elementos del aside cuando se ingresa algun texto
+function hideElement() {
+  if (formTextarea.value !== "") {
+    munhecoImg.style.display = "none";
+    asideTitle.style.display = "none";
+    asideDescription.style.display = "none";
+    asideTextarea.style.display = "block";
+    asideClipboard.style.display = "block";
+  } else {
+    munhecoImg.style.display = "block";
+    asideTitle.style.display = "block";
+    asideDescription.style.display = "block";
+    asideTextarea.style.display = "none";
+    asideClipboard.style.display = "none";
+  }
+}
+
+// Función que copia el texto encriptado o
+// desencriptado al portapapeles
+function copyToClipboard(elementId) {
+  // Obtenemos el elemento pasado a la función
+  const copyText = document.getElementById(elementId);
+  // corroboramos que el elemento tenga algo que copiar
+  if (copyText.value !== "") {
+    // se copia el texto que haya en el el capo de texto
+    navigator.clipboard.writeText(copyText.value);
+    // se manda un alerta con el texto copiado
+    alert(`${copyText.value} was successfully copied to clipboard`);
+    // se corta la ejecución para que no pase al siguiente alert
+    return;
+  }
+  // si no hay nada que copiar en el campo de texto
+  // se envía un alert pertinenete
+  alert("Nothing to copy to clipboard");
+}
+
+encryptButton.onclick = () => encryptMessage(formTextarea);
+decryptButton.onclick = () => decryptMessage(formTextarea);
+asideClipboard.onclick = () => copyToClipboard(asideTextarea.id);
+formTextarea.onchange = hideElement;
+
